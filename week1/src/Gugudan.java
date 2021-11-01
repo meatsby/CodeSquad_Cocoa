3a9d8f1f2377fe45eec94ad5d5bc59d85c6d3ae3
meatsby <meatsby@gmail.com>
2021-11-01 17:13:08 +0900

    Initial commit

---
+++ week1/src/Gugudan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("시작단 : ");
+        int start = scanner.nextInt();
+        System.out.println("끝단 : ");
+        int end = scanner.nextInt();
+
+        for(int i = start; i < end+1; i++) {
+            System.out.println(i + "단");
+            for(int j = 1; j < 10; j++) {
+                System.out.println(i + "*" + j + "=" + i * j);
+            }
+        }
+    }
+}
