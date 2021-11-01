d4391464a298493ee8effb6bf361030e96338ad4
meatsby <meatsby@gmail.com>
2021-11-01 17:26:30 +0900

    21.11.01

---
+++ week1/src/ConditionalStatement1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ConditionalStatement1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int A = s.nextInt();
+        int B = s.nextInt();
+
+        s.close();
+
+        if(A>B) System.out.println(">");
+        else if(A<B) System.out.println("<");
+        else System.out.println("==");
+    }
+}
