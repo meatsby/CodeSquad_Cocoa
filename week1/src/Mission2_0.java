9e58190058d8eae84cfe43063ae6e7af657d8a8e
meatsby <meatsby@gmail.com>
2021-11-08 21:28:28 +0900

    21.11.08

---
+++ week1/src/Mission2_0.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Mission2_0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int a[] = new int[10];
+
+        System.out.println(a);
+
+        String[] members = {"Tany", "Ella", "BC", "Barney", "Konda", "Jhin", "OS", "Sonny", "meatsby", "Bart", "Hanse"};
+
+        System.out.println("몇 명인가요?");
+        int n = sc.nextInt();
+
+    }
+}
