dcfe6518bcce4623de77464e6b53a30cbd520e1c
meatsby <meatsby@gmail.com>
2021-11-09 14:46:58 +0900

    21.11.09

--- week1/src/Main.java
+++ week1/src/Main.java
@@ -1,18 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+
 //코코아 학생 아무나 뽑기
 public class Main {
 
     public static void main(String[] args) {
-        final int n = 3;
         //학생 리스트(배열)를 만들고
         List<String> cocoaList = createList();
+
+        // 키보드로 입력 받기.
+        final int n = input(cocoaList.size());
+
         //잘 섞어서
         shuffle(cocoaList);
+
         //그 중에 n명을 뽑는다.
         printN(cocoaList, n);
+    }
+
+    private static int input(int m) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("몇 명 뽑을까요? (1 - " + m + ")");
+        int n = s.nextInt();
+        s.close();
+        return n;
     }
 
     private static void printN(List<String> s, int n) {
