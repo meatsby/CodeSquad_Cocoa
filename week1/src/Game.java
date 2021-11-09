af89b3818ecd749989d86220a6af8979eea70bcd
meatsby <meatsby@gmail.com>
2021-11-09 16:37:59 +0900

    21.11.09

--- week1/src/Game.java
+++ week1/src/Game.java
@@ -1,18 +1,43 @@
+import javax.management.monitor.MonitorSettingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     private final int WIDTH = 5;
     private final int HEIGHT = 5;
     private final int PLAYER_X = 2;
     private final int PLAYER_Y = 2;
+    private final int MAX_MONSTER = 3;
 
     private Player player;
     private int[][] map;
-    private Monster monster;
+    private List<Monster> monsters;
+
+    public boolean isValidLocation(int x, int y) {
+        if (player.isSamePosition(x, y)) return false;
+        return true;
+    }
 
     public void init() {
         createMap(WIDTH, HEIGHT);
         String name = getNameFromUser();
         player = new Player(name, PLAYER_X, PLAYER_Y);
+        setMonsters();
+    }
+
+    private void setMonsters() {
+        monsters = new ArrayList<>();
+        for (int i = 0; i < MAX_MONSTER; i++) {
+            Position p = generateValidRandomPosition();
+            Monster m = new Monster(i, p);
+            monsters.add(m);
+        }
+    }
+
+    //TODO: must implement!
+    private Position generateValidRandomPosition() {
+        return new Position(1, 1);
     }
 
     //TODO: 키보드 입력 받기
@@ -24,12 +49,16 @@ public class Game {
         map = new int[w][h];
     }
 
-    private void start() {
+    public void start() {
         System.out.println("Game Start");
     }
 
     public void showResult() {
         System.out.println("Thank You.");
+        System.out.println(this.player);
+        for (Monster m : monsters) {
+            System.out.println(m);
+        }
         System.out.println("Your Score: " + player.getScore());
     }
 }
--- week1/src/Monster.java
+++ week1/src/Monster.java
@@ -1,4 +1,19 @@
+import java.util.Random;
+
 public class Monster {
-    private int x;
-    private int y;
+    private Position position;
+    private String name;
+
+    public Monster(int n, Position p) {
+        this.name = "Monster" + n;
+        this.position = new Position(p.getX(), p.getY());
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "position=" + position +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
